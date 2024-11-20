1dcac5f9e21f38a8351e28d0fde042ac34d2715b
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-11-20 18:17:37 +0100

    feat: 04 exercise completed

--- snack-funzioni/04 - saluti/script.js
+++ snack-funzioni/04 - saluti/script.js
@@ -4,10 +4,12 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+const sayHello = 'Ciao';
+const saluto = (hello, people) => hello + ' ' + people;
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(saluto(sayHello, name));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
