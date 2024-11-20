390aaf458c31106d4ae3884fc15314a0da61bb59
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-11-20 18:10:51 +0100

    feat: 03 exercise completed

--- snack-funzioni/03 - conta vocali/script.js
+++ snack-funzioni/03 - conta vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function counterVowels(letter){
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let numberLetter = 0;
+  for (let i = 0; i < letter.length; i++){
+    if (vowels.includes(letter[i])){
+      numberLetter++;
+    }
+  }
+  return numberLetter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log('Il numero di vocali presenti è:', counterVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
