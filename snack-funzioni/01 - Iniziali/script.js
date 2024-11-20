782ecea252a6d222b1f9daa3bc8db3fcaf2c2c42
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-11-20 16:32:52 +0100

    feat: 01 iniziali

--- snack-funzioni/01 - Iniziali/script.js
+++ snack-funzioni/01 - Iniziali/script.js
@@ -2,12 +2,19 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function newArray(array1) {
+  let letterArray = [];
+  let arrayInitial;
+  for (let i = 0; i < array1.length; i++){
+   arrayInitial = array1[i].at(0);
+   letterArray.push(arrayInitial);
+  }
+  return letterArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(newArray(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
