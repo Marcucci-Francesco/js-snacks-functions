d648ca7f5f4ae4b7a6590924890c1c6f0e3d920b
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-11-20 18:34:14 +0100

    feat: bonus exercise completed

--- snack-funzioni/05 - bonus/script.js
+++ snack-funzioni/05 - bonus/script.js
@@ -9,10 +9,23 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+const date = new Date ();
+let hour = date.getHours();
+let saluto;
+function sayHello(hello, name){
+  if (hour >= 0 && hour < 13){
+    hello = 'Buongiorno';
+  } else if (hour >= 13 && hour < 17){
+    hello = 'Buon Pomeriggio';
+  } else {
+    hello = 'Buonasera';
+  }
+  return hello + ' ' + name;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(sayHello(saluto, name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
